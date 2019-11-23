c292c9a572cac249c9c3a85eb5f2b68c90c31b0e
PavelLaktn <pavel.emailbox@gmail.com>
2019-11-23 14:00:06 +0300

    Change http_checker script to use args

--- http_checker.py
+++ http_checker.py
@@ -1,53 +1,60 @@
 #-*- encoding: utf-8 -*-
 #!/usr/bin/python3
-"""Add a script to check if a website is available"""
+
+"""
+Script for checking if an HTTP resource is available
+"""
 
 import urllib.request
-import datetime
-import time
+import datetime, time
 import os
+import argparse
 
 def check(url):
-    '''Tries to open a site. Takes the url as an argument. If the site was opened returns True else False'''
+    # Try to open an HTTP resource. Take the url as an argument. If the resource was opened then return True else False
     try:
         urllib.request.urlopen(url)
-        write_log("Site " + url + " was opened")
+        write_log('resource ' + url + ' was opened')
         return True
     except:
-        write_log("Error to open " + url)
+        write_log('Error to open ' + url)
         return False
 
 def send_sms(sms_api_id, phone, message):
-    '''Sends message to phone using sms.ru api'''
+    # Send message to phone using sms.ru api
     try:
-        url="http://sms.ru/sms/send?api_id=%s&to=%s&text=%s"%(sms_api_id, phone, message)
+        url=f'http://sms.ru/sms/send?api_id={sms_api_id}&to={phone}&text={message}'
         urllib.request.urlopen(url)
-        write_log("Send sms " + url)
+        write_log('Send sms ' + url)
     except:
-        write_log("Error to send message " + message)
+        write_log('Error to send message ' + message)
 
 def write_log(string):
-    '''Writes log file'''
-    if os.path.exists("./http_checker.log"):
-        log_file = open("./http_checker.log", 'a')
-        log_file.write(str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()) + '_' + string + '\n')
-        log_file.close()
-    #print(str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()) + '_' + string + '\n')
+    # Write log file
+    log_file = open('./http_checker.log', 'a')
+    log_file.write(str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()) + '_' + string + '\n')
+    log_file.close()
 
 def main():
-	site_name = 'site'
-    site_url = "http://193.93.*.*/"
-    sms_api_id = "********-****-****-****-************"
-    phone = "8903*******"
-    message = "Site_" + site_name +"_is_not_available"
-    while True:
-        count = 0
-        for n in range(3):
-            if check(site_url):
-                count += 1
-                time.sleep(30)
-        if count == 0:
-            send_sms(sms_api_id, phone, message)
+    # Construct the argument parser and parse command line arguments
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-n', '--resource-name', type=str, required=True, help='HTTP resource name')
+    arg_parser.add_argument('-u', '--url', type=str, required=True, help='HTTP resource url')
+    arg_parser.add_argument('-i', '--api-id', type=str, required=True, help='Api id from sms.ru')
+    arg_parser.add_argument('-p', '--phone', type=str, required=True, help='Phone')
+    arg_parser.add_argument('-c', '--count', type=int, default=3, help='Count of checking before sending a message. Default value is 3')
+    args = vars(arg_parser.parse_args())
 
-if __name__ == "__main__":
+    # Run a checking
+    message = 'Site_' + args['resource_name'] + '_is_not_available'
+    while True:
+        count = args['count']
+        for n in range(count):
+            if not check(args['url']):
+                count -= 1
+            time.sleep(30)
+        if count == 0:
+            send_sms(args['api_id'], args['phone'], message)
+
+if __name__ == '__main__':
     main()
